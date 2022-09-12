4c78fdf2b47fcab3b5a396a5940a263a75c2bf01
James Lyne <jim+github@not-null.co.uk>
2022-09-12 16:22:50 +0100

    Refactor to respect see/use levels. Replace plugin integrations with API

--- SuperVanishBridge-paper/src/main/java/uk/co/notnull/supervanishbridge/SuperVanishBridge.java
+++ SuperVanishBridge-paper/src/main/java/uk/co/notnull/supervanishbridge/SuperVanishBridge.java
@@ -2,12 +2,15 @@ package uk.co.notnull.supervanishbridge;
 
 import de.myzelyam.api.vanish.PlayerVanishStateChangeEvent;
 import de.myzelyam.supervanish.SuperVanish;
+import de.myzelyam.supervanish.VanishPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.nio.ByteBuffer;
 
 
 public class SuperVanishBridge extends JavaPlugin implements Listener {
@@ -27,7 +30,7 @@ public class SuperVanishBridge extends JavaPlugin implements Listener {
 			return;
 		}
 
-		sendPluginMessage(player, event.isVanishing());
+		sendPluginMessage(superVanish.getVanishPlayer(getServer().getPlayer(event.getUUID())));
 	}
 
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
@@ -40,11 +43,16 @@ public class SuperVanishBridge extends JavaPlugin implements Listener {
 				return;
 			}
 
-			sendPluginMessage(player,  superVanish.getVanishPlayer(player).isOnlineVanished());
+			sendPluginMessage(superVanish.getVanishPlayer(player));
 		}, 1L);
 	}
 
-	private void sendPluginMessage(Player player, boolean state) {
-		player.sendPluginMessage(this, "supervanish:statechange", new byte[]{ (byte) (state ? 1 : 0) });
+	private void sendPluginMessage(VanishPlayer vanishPlayer) {
+		byte[] data = ByteBuffer.allocate(9)
+				.put((byte) (vanishPlayer.isOnlineVanished() ? 1 : 0))
+				.putInt(vanishPlayer.getUsePermissionLevel())
+				.putInt(vanishPlayer.getSeePermissionLevel())
+				.array();
+		vanishPlayer.getPlayer().sendPluginMessage(this, "supervanish:statechange", data);
 	}
 }
