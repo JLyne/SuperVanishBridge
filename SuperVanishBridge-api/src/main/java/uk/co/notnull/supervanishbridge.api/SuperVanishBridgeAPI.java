bdd24db887133685f8323afa611e4fb4661f4790
James Lyne <jim+github@not-null.co.uk>
2022-09-13 15:04:26 +0100

    Add API for getting visible player suggestions, for tabcomplete etc

--- SuperVanishBridge-api/src/main/java/uk/co/notnull/supervanishbridge.api/SuperVanishBridgeAPI.java
+++ SuperVanishBridge-api/src/main/java/uk/co/notnull/supervanishbridge.api/SuperVanishBridgeAPI.java
@@ -1,7 +1,10 @@
 package uk.co.notnull.supervanishbridge.api;
 
+import com.velocitypowered.api.command.CommandSource;
 import com.velocitypowered.api.proxy.Player;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.UUID;
 
 @SuppressWarnings("unused")
@@ -13,4 +16,8 @@ public interface SuperVanishBridgeAPI {
 	boolean canSee(Player player1, Player player2);
 
 	boolean canSee(UUID uuid1, UUID uuid2);
+
+	List<Player> getPlayerSuggestions(String query, @Nullable CommandSource source);
+
+	List<String> getUsernameSuggestions(String query, @Nullable CommandSource source);
 }
