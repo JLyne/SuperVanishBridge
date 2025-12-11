f3c40518ceda3917b778b2e6070426b6929d14a2
James Lyne <jim+github@not-null.co.uk>
2025-12-11 16:45:54 +0000

    Update gradle and shadow

--- VanishBridge-api/src/main/java/uk/co/notnull/vanishbridge/api/VanishStateChangeEvent.java
+++ VanishBridge-api/src/main/java/uk/co/notnull/vanishbridge/api/VanishStateChangeEvent.java
@@ -3,8 +3,8 @@ package uk.co.notnull.vanishbridge.api;
 import com.velocitypowered.api.proxy.Player;
 
 public class VanishStateChangeEvent {
-	private Player player;
-	private boolean isVanishing;
+	private final Player player;
+	private final boolean isVanishing;
 
 	public VanishStateChangeEvent(Player player, boolean isVanishing) {
 		this.player = player;
